f93ccc55fed97646c839572c8bbd4f3c4096f7c5
spirit <spirit.business15@gmail.com>
2026-02-25 18:01:48 +1100

    Update ModuleRegistry.java

--- src/main/java/myau/ui/clickgui/ModuleRegistry.java
+++ src/main/java/myau/ui/clickgui/ModuleRegistry.java
@@ -81,7 +81,6 @@ public class ModuleRegistry {
         renderModules.add(Myau.moduleManager.getModule(Trajectories.class));
         renderModules.add(Myau.moduleManager.getModule(Radar.class));
         renderModules.add(Myau.moduleManager.getModule(FPScounter.class));
-        renderModules.add(Myau.moduleManager.getModule(Freelook.class));
         renderModules.sort(comparator);
 
         playerModules = new ArrayList<>();
