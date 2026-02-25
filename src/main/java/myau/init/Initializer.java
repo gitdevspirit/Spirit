08a577c74403b0d677cb924d4549bed690890341
spirit <spirit.business15@gmail.com>
2026-02-25 18:33:53 +1100

    Update Initializer.java

--- src/main/java/myau/init/Initializer.java
+++ src/main/java/myau/init/Initializer.java
@@ -2,8 +2,8 @@ package myau.init;
 
 public class Initializer {
     public Initializer() {
-        System.out.println("OpenMyau+ BUILD BY NESPOLA |Btw U got Ratted (trolled)!");
-        System.out.println("Join the Discord: https://dsc.gg/nespola");
-        System.out.println("Meow! =^._.^= ∫");
+        System.out.println("Spirit Client");
+        System.out.println("Loading...");
+        System.out.println("Loaded!");
     }
 }
