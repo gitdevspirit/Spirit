700c3c2cf5d6cd730d70dc2b1f2d9aa6c8acfafc
spirit <spirit.business15@gmail.com>
2026-02-27 13:07:48 +1100

    Update MixinEntityRenderer.java

--- src/main/java/myau/mixin/MixinEntityRenderer.java
+++ src/main/java/myau/mixin/MixinEntityRenderer.java
@@ -1,6 +1,8 @@
 package myau.mixin;
 
 import myau.Myau;
+import myau.event.EventManager;
+import myau.events.Render3DEvent;
 import myau.module.modules.Autoblock;
 import myau.module.modules.KillAura;
 import net.minecraft.client.renderer.EntityRenderer;
@@ -12,13 +14,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(EntityRenderer.class)
 public class MixinEntityRenderer {
 
+    @Inject(method = "renderWorldPass", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/renderer/EntityRenderer;renderCloudsCheck(Lnet/minecraft/client/renderer/RenderGlobal;F)V",
+            shift = At.Shift.BEFORE
+    ))
+    private void onRenderWorld(int pass, float partialTicks, long timeSlice, CallbackInfo ci) {
+        EventManager.call(new Render3DEvent(partialTicks));
+    }
+
     @Inject(method = "hurtCameraEffect", at = @At("HEAD"), cancellable = true)
     private void onHurtCam(float ticks, CallbackInfo ci) {
-
         KillAura killAura = (KillAura) Myau.moduleManager.modules.get(KillAura.class);
         Autoblock autoblock = (Autoblock) Myau.moduleManager.modules.get(Autoblock.class);
-
-        // If KillAura is enabled AND Autoblock is currently blocking, cancel the hurtcam effect
         if (killAura.isEnabled() && autoblock.isBlocking()) {
             ci.cancel();
         }
