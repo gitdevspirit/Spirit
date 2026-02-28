ad1fbf40be0582cf58b50221887771a3d37450e4
spirit <spirit.business15@gmail.com>
2026-02-28 20:06:26 +1100

    Create GuiColors.java

---
+++ src/main/java/myau/ui/clickgui/GuiColors.java
@@ -0,0 +1,7 @@
+package myau.ui.clickgui;
+
+public class GuiColors {
+    public static final int ACCENT      = 0xFFE991B8;
+    public static final int ACCENT_DIM  = 0x55E991B8;
+    public static final int ACCENT_GLOW = 0x22E991B8;
+}
