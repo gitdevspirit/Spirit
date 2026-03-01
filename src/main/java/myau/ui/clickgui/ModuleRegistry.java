2bb8b4d10e3f10dfe011a3614aa46530a975f052
spirit <spirit.business15@gmail.com>
2026-03-01 13:12:00 +1100

    Update ModuleRegistry.java

--- src/main/java/myau/ui/clickgui/ModuleRegistry.java
+++ src/main/java/myau/ui/clickgui/ModuleRegistry.java
@@ -93,6 +93,7 @@ public class ModuleRegistry {
         playerModules.add(Myau.moduleManager.getModule(InvManager.class));
         playerModules.add(Myau.moduleManager.getModule(InvWalk.class));
         playerModules.add(Myau.moduleManager.getModule(Scaffold.class));
+        playerModules.add(Myau.moduleManager.getModule(Clutch.class));
         playerModules.add(Myau.moduleManager.getModule(AutoBlockIn.class));
         playerModules.add(Myau.moduleManager.getModule(AutoSwap.class));
         playerModules.add(Myau.moduleManager.getModule(SpeedMine.class));
