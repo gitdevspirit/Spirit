84d5e39c2cdfc80c00025a9eafdae4b10062fc02
spirit <spirit.business15@gmail.com>
2026-03-01 13:12:01 +1100

    Update Myau.java

--- src/main/java/myau/Myau.java
+++ src/main/java/myau/Myau.java
@@ -95,6 +95,7 @@ public class Myau {
         moduleManager.modules.put(BackTrack.class,       new BackTrack());
         moduleManager.modules.put(FPScounter.class,      new FPScounter());
         moduleManager.modules.put(Chams.class,           new Chams());
+        moduleManager.modules.put(Chams.class,           new Clutch());
         moduleManager.modules.put(ChestESP.class,        new ChestESP());
         moduleManager.modules.put(ChestStealer.class,    new ChestStealer());
         moduleManager.modules.put(Eagle.class,           new Eagle());
