82a949ee98e79b4480c5d91b19bdae3eb92572b8
spirit <spirit.business15@gmail.com>
2026-03-03 19:50:51 +1100

    Update DiscordRPC.java

--- src/main/java/myau/module/modules/DiscordRPC.java
+++ src/main/java/myau/module/modules/DiscordRPC.java
@@ -13,7 +13,7 @@ import net.minecraft.util.EnumChatFormatting;
 public class DiscordRPC extends Module {
 
     // Replace with your Discord application client ID
-    private static final long CLIENT_ID = 1478309687198875700;
+    private static final long CLIENT_ID = 1478309687198875700L;
 
     private final Minecraft mc = Minecraft.getMinecraft();
     private DiscordIPC ipc;
