60c61ff9cc8c32d7e26020408f8c71f3ea510651
spirit <spirit.business15@gmail.com>
2026-03-04 13:27:04 +1100

    Update Myau.java

--- src/main/java/myau/Myau.java
+++ src/main/java/myau/Myau.java
@@ -155,6 +155,7 @@ public class Myau {
         moduleManager.modules.put(Scaffold.class,        new Scaffold());
         moduleManager.modules.put(AutoBlockIn.class,     new AutoBlockIn());
         moduleManager.modules.put(Spammer.class,         new Spammer());
+        moduleManager.modules.put(SilentAura.class,      new SilentAura());
         moduleManager.modules.put(Pit.class,             new Pit());
         moduleManager.modules.put(Speed.class,           new Speed());
         moduleManager.modules.put(SpeedMine.class,       new SpeedMine());
