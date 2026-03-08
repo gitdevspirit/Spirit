378481646c56bda93744cf8d522cfe2a3011e1d6
spirit <spirit.business15@gmail.com>
2026-03-08 18:21:33 +1100

    Update IAccessorS12PacketEntityVelocity.java

--- src/main/java/myau/mixin/IAccessorS12PacketEntityVelocity.java
+++ src/main/java/myau/mixin/IAccessorS12PacketEntityVelocity.java
@@ -2,7 +2,6 @@ package myau.mixin;
 
 import net.minecraft.network.play.server.S12PacketEntityVelocity;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(S12PacketEntityVelocity.class)
@@ -10,7 +9,7 @@ public interface IAccessorS12PacketEntityVelocity {
     @Accessor("motionX") int getMotionX();
     @Accessor("motionY") int getMotionY();
     @Accessor("motionZ") int getMotionZ();
-    @Mutable @Accessor("motionX") void setMotionX(int motionX);
-    @Mutable @Accessor("motionY") void setMotionY(int motionY);
-    @Mutable @Accessor("motionZ") void setMotionZ(int motionZ);
+    @Accessor("motionX") void setMotionX(int v);
+    @Accessor("motionY") void setMotionY(int v);
+    @Accessor("motionZ") void setMotionZ(int v);
 }
