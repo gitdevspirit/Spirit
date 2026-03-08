4574518d708fbc4bfe508eb29ec360d6f829d87d
spirit <spirit.business15@gmail.com>
2026-03-08 18:17:15 +1100

    Update MixinEntityLivingBase.java

--- src/main/java/myau/mixin/MixinEntityLivingBase.java
+++ src/main/java/myau/mixin/MixinEntityLivingBase.java
@@ -2,6 +2,7 @@ package myau.mixin;
 
 import myau.Myau;
 import myau.event.EventManager;
+import myau.events.JumpEvent;
 import myau.events.StrafeEvent;
 import myau.management.RotationState;
 import myau.module.modules.Jesus;
@@ -14,11 +15,22 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @SideOnly(Side.CLIENT)
 @Mixin(value = {EntityLivingBase.class}, priority = 9999)
 public abstract class MixinEntityLivingBase extends MixinEntity {
+    @Inject(method = {"jump"}, at = @At("HEAD"), cancellable = true)
+    private void onJump(CallbackInfo ci) {
+        if (!((Entity)(Object)this instanceof EntityPlayerSP)) return;
+        if (Myau.moduleManager == null) return;
+        JumpEvent event = new JumpEvent();
+        EventManager.call(event);
+        if (event.isCancelled()) ci.cancel();
+    }
+
     @ModifyVariable(
             method = {"jump"},
             at = @At("STORE"),
