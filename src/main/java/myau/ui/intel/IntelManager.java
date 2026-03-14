54a3796b29f5e965e888293951965fe97721b9ed
spirit <spirit.business15@gmail.com>
2026-03-15 10:36:21 +1100

    Update IntelManager.java

--- src/main/java/myau/ui/intel/IntelManager.java
+++ src/main/java/myau/ui/intel/IntelManager.java
@@ -16,6 +16,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.Map;
 import java.util.HashMap;
