97f8cbbbcfff733c194aa902ed1784e52d2a443c
spirit <spirit.business15@gmail.com>
2026-08-04 14:33:42 +1000

    Update MixinGuiIngameHUD.java

--- src/main/java/myau/mixin/MixinGuiIngameHUD.java
+++ src/main/java/myau/mixin/MixinGuiIngameHUD.java
@@ -1,20 +1,22 @@
 package myau.mixin;
 
+import myau.module.ModuleManager;
+import myau.module.modules.HUD;
 import myau.ui.hud.ArraylistHUD;
-import net.minecraftforge.client.GuiIngameForge;
+import net.minecraft.client.gui.GuiIngame;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(GuiIngameForge.class)
+@Mixin(GuiIngame.class)
 public class MixinGuiIngameHUD {
 
-    private final ArraylistHUD hud = new ArraylistHUD();
-
-    @Inject(method = "func_175180_a", at = @At("TAIL"))
+    @Inject(method = "renderGameOverlay", at = @At("TAIL"))
     private void renderHUD(float partialTicks, CallbackInfo ci) {
-        System.out.println("HUD MIXIN FIRED (TAIL)");
-        hud.render();
+        HUD hud = ModuleManager.INSTANCE.getModule(HUD.class);
+        if (hud != null && hud.isEnabled()) {
+            ArraylistHUD.render(hud);
+        }
     }
 }
