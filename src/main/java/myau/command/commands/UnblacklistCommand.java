c29f120d7548b4c8d8f14709d7f9c7c6b8e0d795
spirit <spirit.business15@gmail.com>
2026-08-06 12:46:28 +1000

    Create UnblacklistCommand.java

---
+++ src/main/java/myau/command/commands/UnblacklistCommand.java
@@ -0,0 +1,28 @@
+package myau.command.commands;
+
+import myau.command.Command;
+import myau.management.BlacklistManager;
+
+public class UnblacklistCommand extends Command {
+
+    public UnblacklistCommand() {
+        super("unblacklist", "unbl");
+        setDescription("Remove a player from your blacklist. Usage: .unblacklist <ign>");
+    }
+
+    @Override
+    public void execute(String[] args) {
+        if (args.length != 1) {
+            reply("&cUsage: &f.unblacklist <ign>");
+            return;
+        }
+
+        boolean removed = BlacklistManager.getInstance().remove(args[0]);
+
+        if (removed) {
+            reply("&a[Blacklist] &fRemoved &c" + args[0] + " &ffrom your blacklist.");
+        } else {
+            reply("&c[Blacklist] &c" + args[0] + " &fwas not on your blacklist.");
+        }
+    }
+}
